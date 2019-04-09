c287dde37f0c371127057b0c6ead610b158d5fd6
Martin Skalsky <martin.skalsky@artin.cz>
2019-04-09 13:04:28 +0200

    new version of the conan file which will download the files from the remote repository

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,26 @@
 from conans import ConanFile, AutoToolsBuildEnvironment
 from conans import tools
-
+import os
 
 class XSensMTIConan(ConanFile):
     name = "xsens_mti"
     version = "2019.0.1.0"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    exports_sources = "xspublic/*"
-
+    
+    def source(self):
+        self.run("git clone https://github.com/Roboauto/conan-xsens_mti_driver.git")
+        self.run("cd conan-xsens_mti_driver && git checkout master")
     def build(self):
-        with tools.chdir("xspublic"):
+        with tools.chdir(os.path.join("conan-xsens_mti_driver","xspublic")):
             env_build = AutoToolsBuildEnvironment(self)
             env_build.make()
 
     def package(self):
-        self.copy("*.h", dst="include", src="xspublic")
+        self.copy("*.h", dst="include", src=os.path.join("conan-xsens_mti_driver","xspublic"))
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
-
+        self.cpp_info.libs = [ 'xscontroller', 'xscommon' ,'xstypes']
+        
